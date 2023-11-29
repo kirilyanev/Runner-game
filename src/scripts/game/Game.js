784df5104e13b4e44d47caa43eb5e13cc580fa05
Kiril Yanev <kl_yanev@abv.bg>
2023-11-29 23:35:16 +0200

    Update Game.js

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,10 +1,24 @@
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
 import { Background } from "./Background.js";
+import { Hero } from "./Hero.js";
+import { Platforms } from "./Platforms.js";
 
 export class Game extends Scene {
     create() {
         this.createBackground();
+        this.createHero();
+        this.createPlatforms();
+    }
+
+    createPlatforms() {
+        this.platfroms = new Platforms();
+        this.container.addChild(this.platfroms.container);
+    }
+
+    createHero() {
+        this.hero = new Hero();
+        this.container.addChild(this.hero.sprite);
     }
 
     createBackground() {
@@ -14,5 +28,6 @@ export class Game extends Scene {
 
     update(dt) {
         this.bg.update(dt);
+        this.platfroms.update(dt);
     }
 }
