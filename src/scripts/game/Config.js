5324c8ff0689b0ac0bbb27d9fad0b252f50da2ea
Kiril Yanev <kl_yanev@abv.bg>
2024-02-17 23:44:56 +0200

    fix: restored bgSpeed

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -2,7 +2,7 @@ import { Game } from "./Game";
 import { Tools } from "../system/Tools";
 
 export const Config = {
-    bgSpeed: 1,
+    bgSpeed: 2,
     platforms: {
         moveSpeed: -1.5,
         ranges: {
